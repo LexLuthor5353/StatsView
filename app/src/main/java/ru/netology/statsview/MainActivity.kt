a143c1dbfe4a14820693c71864ec14df14108d88
Aleksandr Karkhunen <ajlvatanabe@gmail.com>
2026-07-10 13:39:20 +0300

    Smart StatsView and change color

---
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -0,0 +1,22 @@
+package ru.netology.statsview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.netology.statsview.ui.StatsView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        findViewById<StatsView>(R.id.statsView).data = listOf(
+            500F,
+            500F,
+            500F,
+            500F,
+        )
+    }
+}
